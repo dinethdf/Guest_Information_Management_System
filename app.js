94f2d78e17afda751bdf82098e753e0ae683f97f
dineth Dilshan <E2145112@bit.uom.lk>
2023-07-20 15:08:00 +0530

    Adding DB file and App file
    get Idea about the obgect pass one file to othe and how to it call

---
+++ app.js
@@ -0,0 +1,8 @@
+const db = require("./gustdb.js");
+
+//Data input
+console.log("App.js")
+console.log(db.addx());
+console.log(db.uptx());
+
+
---
+++ gustdb.js
@@ -0,0 +1,18 @@
+// Data Save
+console.log("Database DB")
+const Name = "Dineth";
+
+const add =() => {
+    return "Adding Now"
+}
+
+
+const upt =() => {
+    return "Updating Now"
+}
+
+module.exports = {
+    addx:add,
+    uptx:upt
+};
+    
