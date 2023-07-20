5b51514588c80eb7fefe90b776d79c387f5dcee6
dineth Dilshan <E2145112@bit.uom.lk>
2023-07-20 14:15:18 +0530

    Lessen 15
    Use Library Os and File Using updates and system performance find

--- test/core_test.js
+++ test/core_test.js
@@ -1,3 +1,8 @@
 const fs = require('fs');
+const os = require('os');
 
-fs.writeFileSync("data.txt","Hellow World");
+fs.writeFileSync("data.txt","Hellow World ");
+fs.appendFileSync("data.txt", " My Worls")
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.hostname());
