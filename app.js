61d576fbed4b31c3f10bb8ddfb96b1efd0c3ffdd
dineth Dilshan <E2145112@bit.uom.lk>
2023-07-26 11:09:33 +0530

    New add files and fuctions

--- app.js
+++ app.js
@@ -1,43 +1,115 @@
 const yargs = require("yargs");
-const db = require("./gustdb");
+const db=require("./guestdb.js");
 
-//gust -id, name , address , contact_no , visit_date 
+//guest -id, name , address, contact_no ,visit_date
 
 yargs.version("1.0.1");
 
+//add
 yargs.command({
     command:'add',
-    describe: 'Add a Guest',
+    describe: 'Add a guest',
     builder:{
-        name: {
-            describe: "Add a gues",
-            demandOption: true,
+       name:{
+           describe:"Name",
+           demandOption:true,
+           type:"string"
+       },
+       address:{
+           describe:"Address", 
+           demandOption:true,
+           type:"string"
+       },
+       contact_no:{
+            describe:"Contact No",
+            demandOption:true,
+            type:"number"
+       },
+       visit_date:{
+            describe:"Visit Date",
+            demandOption:true,
             type:"string"
-        },
-        address: {
-            describe: "Address",
-            demandOption: true,
-            type:"string"
-        },
-        contact_no: {
-            describe: "Mob Number",
-            demandOption: true,
-            type:"string"
-        },
-        visit_date: {
-            describe: "Come date",
-            demandOption: true,
-            type:"string"
-        }
-
+        }  
     },
     handler(argv){
-        db.add(argv.name);
-
+        db.addGuest(argv.name,argv.address,argv.contact_no,argv.visit_date);
     }
 })
-//console.log(process.argv);
+
+//update
+yargs.command({
+    command:'update',
+    describe: 'Update a guest',
+    builder:{
+        id:{
+            describe:"ID",
+            demandOption:true,
+            type:"number"
+        },
+        name:{
+            describe:"Name",
+            type:"string"
+        },
+        address:{
+            describe:"Address",
+            type:"string"
+        },
+        contact_no:{
+            describe:"Contact No",
+            type:"number"
+         },
+         visit_date:{
+            describe:"Visit Date",
+            type:"string"
+         }  
+    },
+    handler(argv){
+        db.updateGuest(argv.id,argv.name,argv.address,argv.contact_no,argv.visit_date);
+    }
+});
+
+
+//delete
+yargs.command({
+    command:'delete',
+    describe: 'Delete a guest',
+    builder:{
+        id:{
+            describe:"ID",
+            demandOption:true,
+            type:"number"
+        }
+    },
+    handler(argv){
+        db.deleteGuest(argv.id);
+    }
+});
+
+//read
+yargs.command({
+    command:'read',
+    describe: 'read a guest',
+    builder:{
+        id:{
+            describe:"ID",
+            demandOption:true,
+            type:"number"
+        }
+    },
+    handler(argv){
+        db.readGuest(argv.id);
+    }
+});
+
+//list
+yargs.command({
+    command:'list',
+    describe: 'List all guests',
+    handler(){
+        db.listGuest();
+    }
+});
+
 yargs.parse();
-//console.log(yargs.argv); 
 
 
--- test/core_test.js
+++ test/core_test.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
-const os = require('os');
+// const os = require('os');
+const db_file="data.json";
 
-fs.writeFileSync("data.txt","Hellow World ");
-fs.appendFileSync("data.txt", " My Worls")
-console.log(os.freemem());
-console.log(os.totalmem());
-console.log(os.hostname());
+// fs.writeFileSync("data.txt","Hellow World ");
+// fs.appendFileSync("data.txt", " My Worls")
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.hostname());
+
+const dataBuffer = fs.readFileSync(db_file);
+        const dataJSON = dataBuffer.toString();
+        console.log(JSON.parse(dataJSON));
